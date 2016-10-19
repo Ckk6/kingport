3961e190c706fd1b0c2f643b9c879a20449e6750
CKK6 <Tresk26@yahoo.com>
2016-10-19 11:26:46 -0400

    routing update

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static('public'));
+
+//connect to static folders
+app.use(express.static('./public/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//require collections
+
 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log('connected to', port);
+});
+
+//homepage or root (html)
+app.get('/', function(req,res){
+	res.send(index.html);
 });
